2d31a954fd16765edd6e2dfb535fff4a8e84cdbc
stanislav <xiiiovart@gmail.com>
2024-05-14 14:08:44 +0300

    EventSelect

--- src/components/Coins/Series/Series.tsx
+++ src/components/Coins/Series/Series.tsx
@@ -1,16 +1,25 @@
-import React from 'react';
+import { useState } from 'react';
 import { LengthRange } from './components/Range/LengthRange';
 import { SerialsRange } from './components/Range/SerialsRange';
 import { useDiceSeries } from '../../../state/useDiceSeries';
+import { Button } from '../../Button/Button';
+import { EventSelect } from './components/Select/EventSelect';
 
 import styles from './Series.module.scss';
 
 export const DiceSeries = () => {
+  const [disabled, setDisabled] = useState(false);
   const length = useDiceSeries(s => s.length);
   const seriesN = useDiceSeries(s => s.seriesN);
+  const event = useDiceSeries(s => s.event);
+
+  const startHandler = () => {
+    console.log('START');
+  }
+
   return (
     <div className={styles.wrapper}>
-      {`length: ${length}, seriesN: ${seriesN}`}
+      {`length: ${length}, seriesN: ${seriesN}, event: ${event}`}
       <h2 className={styles.title}>Серии бросков монеты</h2>
       <div className={styles.controllWrapper}>
         <div>
@@ -20,6 +29,18 @@ export const DiceSeries = () => {
         <div>
           <h3 className={styles.subTitle}>Количество серий</h3>
           <SerialsRange />
+        </div>
+        <Button
+          title={`Запустить`}
+          disabled={disabled}
+          cb={startHandler}
+          setDisabled={setDisabled}
+          timeout={1000}
+        />
+      </div>
+      <div className={styles.controllWrapper}>
+        <div>
+          <EventSelect />
         </div>
       </div>
     </div>
---
+++ src/components/Coins/Series/components/Select/EventSelect.tsx
@@ -0,0 +1,25 @@
+import { ChangeEvent } from 'react';
+import { Coin, useDiceSeries } from '../../../../../state/useDiceSeries';
+
+import styles from './Select.module.scss'
+
+export const EventSelect = () => {
+  const setEvent = useDiceSeries(s => s.setEvent);
+  const selectHandler = (e: ChangeEvent<HTMLSelectElement>) => {
+    setEvent(e.target.value as Coin);
+  }
+  return (
+    <label className={styles.label} htmlFor="selectType">
+      Событие:
+      <select
+        id='selectType'
+        className={styles.select}
+        onChange={selectHandler}
+        defaultValue={'5'}
+      >
+        <option value={'OREL'}>Орёл</option>
+        <option value={'5'}>Решка</option>
+      </select>
+    </label>
+  );
+}
